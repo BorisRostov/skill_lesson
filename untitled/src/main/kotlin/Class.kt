25b41415182ad06020810ea6d77e7f3d3658de2b
BorisRostov <70660836+BorisRostov@users.noreply.github.com>
2022-11-11 18:41:04 +0300

    abcd

---
+++ untitled/src/main/kotlin/Class.kt
@@ -0,0 +1,10 @@
+import java.util.Random
+
+class Class {
+
+    var a = MutableList(10){(0..100).random()}
+//    Class().a.forEach {
+//        println("print $it")
+//    }
+
+}
--- untitled/src/main/kotlin/Main.kt
+++ untitled/src/main/kotlin/Main.kt
@@ -1,9 +1,4 @@
 fun main(args : Array<String>) {
     println("Hello World!")
 
-    for (i in 1..10){
-        println("print $i")
-
-    }
-
 }
